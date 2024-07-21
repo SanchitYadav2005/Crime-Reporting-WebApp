99f51d429a6739521a01fed0d556297b0bdbcc2c
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-07-21 09:36:02 +0530

    installed cors and connected the database

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,29 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require("express")
-const app = express()
-const port = 8080
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-app.get('/',(req,res)=>{
-    res.send("hello world!!")
-})
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, ()=>{
-    console.log(`listning on ${port}`)
-})
+app.get("/", (req, res) => {
+  res.send("hello world!!");
+});
+console.log(process.env.MONGODB_URL)
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and server!!");
+      
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
