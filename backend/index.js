af85bec7cbefed3c59dd22a6a9301ecbfd68a6c0
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-08-08 21:48:29 +0530

    installed axios

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
-
+//all packages
 const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
+//routes and all other imports
+const Routes = require('./routes/routes')
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-
+//routes
+app.use('/', Routes)
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,5 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "your app is working fine!! lets build it" });
+  res.status(200).json({ message: "wokring" });
 });
+
+
+module.exports = router;
