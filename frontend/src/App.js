f8c8afe1a734209066a71240fd36ada304573e1b
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-08-08 21:46:53 +0530

    confinguring react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 
-
+import Navbar from './components/Navbar';
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(()=>{
+    const getData = async ()=>{
+      const res = await axios.get("http://localhost:8080/");
+      if(res){
+        setData(res.data)
+        console.log(data)
+      }
+    }
+    getData()
+  },[])
   return (
     <div className="App">
-      
+      <Navbar/>
     </div>
   );
 }
