1fe9944ff4c81972e04fd25b98f45332b68bfb98
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-08-12 20:06:54 +0530

    icons working

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { PhoneIcon } from "@heroicons/24/outline";
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { PhoneIcon } from "@heroicons/react/24/outline";
 
 function Navbar() {
   return (
     <nav>
-        <div>Alertify</div>
-        <ul>
-            <li>Report</li>
-            <li>Contact</li>
-            <li>ALL Reports</li>
-        </ul>
-        <div>
-            <Link to="login">login</Link>
-            <PhoneIcon/>
-        </div>
+      <div>Alertify</div>
+      <ul>
+        <li>Report</li>
+        <li>Contact</li>
+        <li>ALL Reports</li>
+      </ul>
+      <div>
+        <Link to="login">login</Link>
+        <PhoneIcon />
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
