1ae06d3c63459624c58db006cb6f035719b57b5c
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-08-13 23:38:22 +0530

    done with main stuff for navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,19 +5,25 @@ import icon from "../assests/icon.png";
 
 function Navbar() {
   return (
-    <nav className="flex items-center justify-evenly mt-2 ms-2 mr-2 bg-background-nav p-5 text-white rounded-lg">
-      <div className="text-xl hover:text-logo-color hover:cursor-pointer decoration-solid">
+    <nav className="flex items-center justify-evenly mt-2 ms-2 mr-2  p-5 text-white rounded-lg">
+      <div className="text-xl hover:text-hover_color hover:cursor-pointer decoration-solid">
         <img className="w-2 inline mr-2" src={icon} alt="icon" />
         Alertify
       </div>
       <ul className="flex">
-        <li>Report</li>
-        <li className="ms-12">Contact</li>
-        <li className="ms-12">All Reports</li>
+        <li className="hover:text-hover_color">
+          <Link to={"/report"}>Report</Link>
+        </li>
+        <li className="ms-12 hover:text-hover_color ">
+          <Link to={"/contact"}>Contact</Link>
+        </li>
+        <li className="ms-12 hover:text-hover_color">
+          <Link to={"/allreports"}>All Reports</Link>
+        </li>
       </ul>
       <div className="flex justify-evenly items-center">
         <Link to="login">login</Link>
-        <PhoneIcon width={50} height={20} />
+        <PhoneIcon className="phone_logo" width={50} />
       </div>
     </nav>
   );
