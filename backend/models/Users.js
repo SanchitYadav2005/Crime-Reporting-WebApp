e2bc83a3b97ea2083e7cb982f3a58bd52818bcd0
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-04 21:23:59 +0530

    changed the schmea with more options

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,12 +1,37 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema({
-    username:String,
-    firstname:String,
-    lastname:String,
-    email:String,
-    phone:Number,
-    password:String
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  firstname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlenght: 0,
+  },
+});
 
-module.exports = mongoose.model('User', UserSchema)
+module.exports = mongoose.model("User", UserSchema);
