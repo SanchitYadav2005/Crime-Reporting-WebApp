8a54b5ef8be505b23f970c3c36f9a4c00a13f6bb
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-06 11:19:20 +0530

    done with the setting and downloaded bcrypt and jsonwebtoken

--- backend/index.js
+++ backend/index.js
@@ -5,22 +5,30 @@ const app = express();
 const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
-//routes and all other imports
-const Routes = require('./routes/routes')
 
+
+
+//routes and all other imports
+const Routes = require("./routes/routes");
+const userRoutes = require("./routes/userRoutes");
+
+//configuration
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+
 
 //routes
-app.use('/', Routes)
+app.use("/", Routes);
+app.use("/api/user", userRoutes);
+
+//connecting to the DB
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => {
     app.listen(process.env.PORT, () => {
       console.log("connected to db and server!!");
-      
     });
   })
   .catch((error) => {
--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
 
 const UserSchema = mongoose.Schema({
   username: {
@@ -33,5 +35,47 @@ const UserSchema = mongoose.Schema({
     minlenght: 0,
   },
 });
+UserSchema.statics.signup = async function (
+  email,
+  username,
+  firstname,
+  lastname,
+  phone,
+  password
+) {
+  const User = this;
+  const exists = await User.findOne({ email });
+  if (exists) {
+    throw new Error("User already exists!!");
+  }
+  if (!email || !password || !username || !firstname || !lastname || !phone) {
+    throw new Error("All fields must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email !!");
+  }
+  if (
+    !validator.isStrongPassword(password, {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    })
+  ) {
+    throw new Error("Password is not strong enough!");
+  }
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await User.create({
+    email,
+    password: hash,
+    username,
+    firstname,
+    lastname,
+    phone,
+  });
+  return user;
+};
 
 module.exports = mongoose.model("User", UserSchema);
