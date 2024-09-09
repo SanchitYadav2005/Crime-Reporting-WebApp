ade4661630ca1c7620c9f861f5ab59d49be9fd93
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-09 09:09:58 +0530

    added login route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,8 @@
-const express = require("express")
-const router = express.Router()
-const {createUser} = require('../controllers/userController')
+const express = require("express");
+const router = express.Router();
+const { createUser, loginUser } = require("../controllers/userController");
 
-
-router.post('/signup', createUser)
-
+router.post("/signup", createUser);
+router.post("/login", loginUser);
 
 module.exports = router;
