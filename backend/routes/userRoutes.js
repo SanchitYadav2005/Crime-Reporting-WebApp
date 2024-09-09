5fbbd9a549f70c7a1e77e3b5d9b06171fdb3a043
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-09 09:36:10 +0530

    added route for logout

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, loginUser } = require("../controllers/userController");
+const {
+  createUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/userController");
 
 router.post("/signup", createUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 
 module.exports = router;
