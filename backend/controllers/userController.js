646ec001c5a17209512eb45c0e9e8e6c24893bdf
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-09 09:09:12 +0530

    done with login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,9 @@ const createToken = async (_id) => {
   await jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
 };
 
+// creation of user
 module.exports.createUser = async (req, res) => {
+  // all the fields
   const { email, username, firstname, lastname, phone, password } = req.body;
   try {
     const user = await User.signup(
@@ -17,16 +19,27 @@ module.exports.createUser = async (req, res) => {
       phone,
       password
     );
-
-    const token = await createToken(user._id);
-    res
-      .status(200)
-      .json({
-        message: "user created !",
-        user: { email, username, firstname, lastname, phone },
-        token,
-      });
+    // token creation
+    const token = createToken(user._id);
+    res.status(200).json({
+      message: "user created !",
+      user: { email, username, firstname, lastname, phone },
+      token,
+    });
   } catch (error) {
-    console.log(error);
+    res.status(400).json({ message: error });
+  }
+};
+
+// login funciton
+
+module.exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ message: "logged in successfully" }, user, token);
+  } catch (error) {
+    res.status(error.status).json({ message: error });
   }
 };
