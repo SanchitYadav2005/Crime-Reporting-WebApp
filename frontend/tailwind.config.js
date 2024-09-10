27df62fc80d285e94ce84f642e646044fd5fdede
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-10 20:36:15 +0530

    added one more custom color in tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
       colors: {
         'logo-color': "#caf0f8",
         'hover_color': '#dee2e6',
-        'text_color': "#219ebc"
+        'text_color': "#219ebc",
+        'footer_color':"#8d99ae"
       },
       spacing: {
         'mt-120': '15em',
