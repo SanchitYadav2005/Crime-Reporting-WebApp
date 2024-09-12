965658a803cfb0b4ef92c8666de02d1aeebbf0ae
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-12 10:41:01 +0530

    done with reducer

---
+++ frontend/src/reducer/userReducer.js
@@ -0,0 +1,10 @@
+export const userReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return { user: action.payload };
+    case "LOGOUT":
+      return { user: null };
+    default:
+      return state;
+  }
+};
