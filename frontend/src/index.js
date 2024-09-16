d1792d2993e995af625f7727d8bc54a5a1501d37
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2024-09-16 10:33:26 +0530

    added context as a parent

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { UserContextProvider } from "./context/UserContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
